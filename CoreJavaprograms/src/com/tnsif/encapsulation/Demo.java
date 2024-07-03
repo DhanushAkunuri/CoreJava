98ab356ee783242d148244692f024cf89a5d7203
AMRUTH <AMRUTH@Amruth>
2024-07-03 15:35:18 +0530

    commit message

---
+++ CoreJavaprograms/src/com/tnsif/encapsulation/Demo.java
@@ -0,0 +1,11 @@
+package com.tnsif.encapsulation;
+
+public class Demo {
+public static void main(String[] args) {
+	Encap e=new Encap();
+			e.name="dhanush";
+			e.serialnumber=1;
+			e.age=18;
+			e.show();
+}
+}
---
+++ CoreJavaprograms/src/com/tnsif/encapsulation/Encap.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class Encap {
+
+	int serialnumber;
+	String name;
+	int age;
+	
+	void show()
+	{
+		System.out.println("Serial number "+serialnumber+" name "+name+" age "+age);
+		
+	}
+}
