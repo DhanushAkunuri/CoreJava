049a695a2fcbfc0b9f0476d803a152e57d158e48
AMRUTH <AMRUTH@Amruth>
2024-07-03 13:59:49 +0530

    commit message

---
+++ CoreJavaprograms/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.packages;
+
+public class Publicdemo {
+
+	public int a=8;
+	public static void main(String[] args) {
+		Publicdemo obj = new Publicdemo();
+		
+		System.out.println(obj.a);
+				
+	}
+}
---
+++ CoreJavaprograms/src/com/tnsif/scannerclass/Scannerexample.java
@@ -0,0 +1,18 @@
+package com.tnsif.scannerclass;
+
+import java.util.Scanner;
+
+//demo for scanner
+public class Scannerexample {
+public static void main(String[] args) {
+	
+Scanner sc=new Scanner(System.in);
+
+System.out.println("enter your name");
+String name=sc.nextLine();
+
+System.out.println("enter the roll number");
+int roll=sc.nextInt();
+System.out.println("name "+name+" and roll "+roll);
+}
+}
