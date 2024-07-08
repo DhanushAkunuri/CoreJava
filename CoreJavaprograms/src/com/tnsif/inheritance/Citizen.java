9074dde4179b4b4e35aeeecc0b47f5d68ec36c65
AMRUTH <AMRUTH@Amruth>
2024-07-08 21:54:44 +0530

    commit message

--- CoreJavaprograms/src/com/tnsif/inheritance/Citizen.java
+++ CoreJavaprograms/src/com/tnsif/inheritance/Citizen.java
@@ -7,7 +7,6 @@ public class Citizen {
 	private long phno;
 	
 	public Citizen(String name, long adharno, String address,long phno) {
-		super();
 		this.name = name;
 		this.adharno = adharno;
 		this.address = address;
